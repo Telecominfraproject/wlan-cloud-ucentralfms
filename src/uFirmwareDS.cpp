08bb2b8a1ac7c633b6d968fac3f59d33a86b84af
stephb9959 <stephane.bourque@gmail.com>
2021-05-15 18:09:38 -0700

    Initial

--- src/uFirmwareDS.cpp
+++ src/uFirmwareDS.cpp
@@ -282,6 +282,10 @@ int main(int argc, char **argv) {
         DBGLINE
 
         Aws::SDKOptions options;
+        options.memoryManagementOptions.memoryManager = nullptr;
+        options.httpOptions.initAndCleanupCurl = true;
+        options.cryptoOptions.initAndCleanupOpenSSL = true;
+        options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Trace;
         DBGLINE
         Aws::InitAPI(options);
         DBGLINE
