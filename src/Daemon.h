9851a2f350b290ab3767459acc55174deb44d994
stephb9959 <stephane.bourque@gmail.com>
2021-06-13 23:44:21 -0700

    Initial

--- src/Daemon.h
+++ src/Daemon.h
@@ -57,32 +57,33 @@ namespace uCentral {
         void InitializeSubSystemServers();
         void StartSubSystemServers();
         void StopSubSystemServers();
-        static std::string Version();
-        const Poco::SharedPtr<Poco::Crypto::RSAKey> & Key() { return AppKey_; }
-        std::string CreateUUID();
         static void Exit(int Reason);
-        [[nodiscard]] inline const std::string & DataDir() { return DataDir_; }
 
         bool SetSubsystemLogLevel(const std::string & SubSystem, const std::string & Level);
+        [[nodiscard]] static std::string Version();
+        [[nodiscard]] const Poco::SharedPtr<Poco::Crypto::RSAKey> & Key() { return AppKey_; }
+        [[nodiscard]] inline const std::string & DataDir() { return DataDir_; }
+        [[nodiscard]] std::string CreateUUID();
+        [[nodiscard]] bool Debug() const { return DebugMode_; }
+        [[nodiscard]] uint64_t ID() const { return ID_; }
         [[nodiscard]] Types::StringVec GetSubSystems() const;
         [[nodiscard]] Types::StringPairVec GetLogLevels() const;
         [[nodiscard]] const Types::StringVec & GetLogLevelNames() const;
-
-        uint64_t ConfigGetInt(const std::string &Key,uint64_t Default);
-        uint64_t ConfigGetInt(const std::string &Key);
-        std::string ConfigGetString(const std::string &Key,const std::string & Default);
-        std::string ConfigGetString(const std::string &Key);
-        uint64_t ConfigGetBool(const std::string &Key,bool Default);
-        uint64_t ConfigGetBool(const std::string &Key);
+        [[nodiscard]] std::string ConfigGetString(const std::string &Key,const std::string & Default);
+        [[nodiscard]] std::string ConfigGetString(const std::string &Key);
+        [[nodiscard]] uint64_t ConfigGetInt(const std::string &Key,uint64_t Default);
+        [[nodiscard]] uint64_t ConfigGetInt(const std::string &Key);
+        [[nodiscard]] uint64_t ConfigGetBool(const std::string &Key,bool Default);
+        [[nodiscard]] uint64_t ConfigGetBool(const std::string &Key);
 
     private:
         static Daemon          *instance_;
-        bool     HelpRequested_ = false;
+        bool                    HelpRequested_ = false;
         std::string             LogDir_;
         std::string             ConfigFileName_;
         Poco::UUIDGenerator     UUIDGenerator_;
         MyErrorHandler          AppErrorHandler_;
-        uint64_t                    ID_ = 1;
+        uint64_t                ID_ = 1;
         Poco::SharedPtr<Poco::Crypto::RSAKey>	AppKey_ = nullptr;
         bool                    DebugMode_ = false;
         std::string 			DataDir_;
