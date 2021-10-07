8f30237933f7d19563e31362c24ed3d0fcdb4fb1
stephb9959 <stephane.bourque@gmail.com>
2021-10-06 22:39:01 -0700

    Adding autoupdating via provisioning

--- src/AutoUpdater.cpp
+++ src/AutoUpdater.cpp
@@ -40,49 +40,53 @@ namespace OpenWifi {
             while(!Queue_.empty() && Running_) {
                 auto Entry = Queue_.front();
                 Queue_.pop_front();
-                std::cout << "Preparing to upgrade " << Entry.first << std::endl;
-                auto CacheEntry = Cache_.find(Entry.first);
-                uint64_t Now = std::time(nullptr);
-                if(CacheEntry == Cache_.end() || (CacheEntry->second.LastCheck-Now)>300) {
-                    //  get the firmware settings for that device.
-                    std::string firmwareUpgrade;
-                    bool        firmwareRCOnly;
-                    SerialCache     C;
-                    C.LastCheck = Now;
-                    std::cout << "Retrieving firmware options for " << Entry.first << std::endl;
-                    if(OpenWifi::SDK::Prov::GetFirmwareOptions(Entry.first, firmwareUpgrade, firmwareRCOnly)) {
-                        std::cout << "Retrieved options for " << Entry.first << std::endl;
-                        C.firmwareRCOnly = firmwareRCOnly;
-                        C.firmwareUpgrade = firmwareUpgrade;
-                    } else {
-                        std::cout << "Could not retrieve options for " << Entry.first << std::endl;
-                        C.firmwareRCOnly = firmwareRCOnly;
-                        C.firmwareUpgrade = firmwareUpgrade;
-                    }
-                    Cache_[Entry.first] = C;
-                } else {
-
-                }
-                std::cout << "Continuing... " << std::endl;
-                LatestFirmwareCacheEntry    fwEntry;
-                FMSObjects::Firmware        fwDetails;
-                std::cout << "Checking last firmware..." << std::endl;
-                auto LF = LatestFirmwareCache()->FindLatestFirmware(Entry.second, fwEntry );
-                if(LF) {
-                    std::cout << "Getting id:" << fwEntry.Id << std::endl;
-                    if(Storage()->GetFirmware(fwEntry.Id,fwDetails)) {
-                        //  send the command to upgrade this device...
-                        std::cout << "Upgrading " << Entry.first << " to version " << fwDetails.uri << std::endl;
-                        if(OpenWifi::SDK::GW::SendFirmwareUpgradeCommand(Entry.first,fwDetails.uri)) {
-                            std::cout << "Upgrade command sent ... for " << Entry.first << std::endl;
+                try {
+                    std::cout << "Preparing to upgrade " << Entry.first << std::endl;
+                    auto CacheEntry = Cache_.find(Entry.first);
+                    uint64_t Now = std::time(nullptr);
+                    if(CacheEntry == Cache_.end() || (CacheEntry->second.LastCheck-Now)>300) {
+                        //  get the firmware settings for that device.
+                        std::string firmwareUpgrade;
+                        bool        firmwareRCOnly;
+                        SerialCache     C;
+                        C.LastCheck = Now;
+                        std::cout << "Retrieving firmware options for " << Entry.first << std::endl;
+                        if(OpenWifi::SDK::Prov::GetFirmwareOptions(Entry.first, firmwareUpgrade, firmwareRCOnly)) {
+                            std::cout << "Retrieved options for " << Entry.first << std::endl;
+                            C.firmwareRCOnly = firmwareRCOnly;
+                            C.firmwareUpgrade = firmwareUpgrade;
                         } else {
-                            std::cout << "Upgrade command NOT sent ... for " << Entry.first << std::endl;
+                            std::cout << "Could not retrieve options for " << Entry.first << std::endl;
+                            C.firmwareRCOnly = firmwareRCOnly;
+                            C.firmwareUpgrade = firmwareUpgrade;
+                        }
+                        Cache_[Entry.first] = C;
+                    } else {
+
+                    }
+                    std::cout << "Continuing... " << std::endl;
+                    LatestFirmwareCacheEntry    fwEntry;
+                    FMSObjects::Firmware        fwDetails;
+                    std::cout << "Checking last firmware..." << std::endl;
+                    auto LF = LatestFirmwareCache()->FindLatestFirmware(Entry.second, fwEntry );
+                    if(LF) {
+                        std::cout << "Getting id:" << fwEntry.Id << std::endl;
+                        if(Storage()->GetFirmware(fwEntry.Id,fwDetails)) {
+                            //  send the command to upgrade this device...
+                            std::cout << "Upgrading " << Entry.first << " to version " << fwDetails.uri << std::endl;
+                            if(OpenWifi::SDK::GW::SendFirmwareUpgradeCommand(Entry.first,fwDetails.uri)) {
+                                std::cout << "Upgrade command sent ... for " << Entry.first << std::endl;
+                            } else {
+                                std::cout << "Upgrade command NOT sent ... for " << Entry.first << std::endl;
+                            }
+                        } else {
+                            std::cout << "Could not find the desired firmware" << std::endl;
                         }
                     } else {
-                        std::cout << "Could not find the desired firmware" << std::endl;
+                        std::cout << "Cannot find latest firmware..." << std::endl;
                     }
-                } else {
-                    std::cout << "Cannot find latest firmware..." << std::endl;
+                } catch (...) {
+                    std::cout << "Exception during upgrade loop." << std::endl;
                 }
             }
         }
