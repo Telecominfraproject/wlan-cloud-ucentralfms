2a90955d3c23a4941915c47b4aef8a25cf6d622e
stephb9959 <stephane.bourque@gmail.com>
2022-03-31 10:13:03 -0700

    Fixing default syntax for bool.

--- src/RESTObjects/RESTAPI_FMSObjects.h
+++ src/RESTObjects/RESTAPI_FMSObjects.h
@@ -29,7 +29,7 @@ namespace OpenWifi::FMSObjects {
         std::string     location;
         std::string     uploader;
         std::string     digest;
-        bool            latest=0;
+        bool            latest=false;
         SecurityObjects::NoteInfoVec    notes;
         uint64_t        created=0;
 
